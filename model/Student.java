8bb23e3dc93a68d4211283523919bd4ed5688d6d
IrinkaCh <123irina00@mail.ru>
2023-01-19 18:59:48 +0500

    model

---
+++ model/Student.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Student extends StudentAbstract {
+    private String name;
+    private int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    @Override
+    public String toString() {
+        return "\nStudent [name=" + name + ", course=" + course + "]";
+    }
+}
---
+++ model/StudentAbstract.java
@@ -0,0 +1,7 @@
+package model;
+
+public abstract class StudentAbstract implements StudentInterface {
+
+    public String name;
+    public int course;
+}
